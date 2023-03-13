32ea6815f47dac531f0b9a5d0b92226fa24a44fc
ppongpeera <ppongpeera@gmail.com>
2023-03-13 16:24:46 +0700

    my test fn

---
+++ pracetice.js
@@ -0,0 +1,19 @@
+const addab = (a, b) => a + b;
+const subab = (a, b) => a - b;
+const mulab = (a, b) => a * b;
+const divab = (a, b) => a / b;
+
+const myInfo = function (firstName, location, hobby) {
+    console.log("Hi, my name is  "+ firstName + "I live in " + location + "and enjoy "+hobby)
+}
+
+//  test function myInfo
+
+myInfo('Pongpeera', 'Bangkok', 'Sleeping');
+
+// test function a,b
+
+addab(6,5);
+subab(6,5);
+mulab(6,5);
+divab(65,5);
